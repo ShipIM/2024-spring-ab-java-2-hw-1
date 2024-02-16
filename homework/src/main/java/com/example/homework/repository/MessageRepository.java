340671c809bf2a75c8784e9e0d13ed33ea87c5a2
ShipIM <ilya.shipunov@gmail.com>
2024-02-16 13:24:16 +0300

    revert

--- homework/src/main/java/com/example/homework/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.homework.repository;
-
-import com.example.homework.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
