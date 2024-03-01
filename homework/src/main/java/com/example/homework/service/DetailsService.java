2baf21c984946241a9d056a1bf32ab78107d6272
ShipIM <ilya.shipunov@gmail.com>
2024-03-02 02:48:02 +0300

    user details caching

--- homework/src/main/java/com/example/homework/service/DetailsService.java
+++ homework/src/main/java/com/example/homework/service/DetailsService.java
@@ -1,22 +1,42 @@
 package com.example.homework.service;
 
+import com.example.homework.model.entity.jpa.User;
+import com.example.homework.model.entity.mongo.Operation;
+import com.example.homework.model.entity.mongo.enumeration.OperationType;
 import com.example.homework.repository.jpa.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class DetailsService implements UserDetailsService {
 
+    private final OperationService operationService;
+
     private final UserRepository userRepository;
 
     @Override
+    @Cacheable(value = "DetailsService::loadUserByUsername", key = "#username")
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new EntityNotFoundException("There is no user with that name"));
+
+        operationService.logOperation(
+                Operation.builder()
+                        .message(String.format("User read from database: %s", username))
+                        .time(LocalDateTime.now())
+                        .type(OperationType.READ)
+                        .build()
+        );
+
+        return user;
     }
+
 }
